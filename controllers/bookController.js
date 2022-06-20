851248cc4322502a482b57278fb8a0688666ed93
Budescu Eftimie <budescueftimie@gmail.com>
2022-06-20 04:59:36 +0200

    test: add change to test outcome on aws server

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -6,7 +6,7 @@ exports.index = function(req, res) {
 
 // Display list of all books.
 exports.book_list = function(req, res) {
-    res.send('NOT IMPLEMENTED: Book list');
+    res.send('NOT IMPLEMENTED: Book list', 'the node env is :', process.env.NODE_ENV);
 };
 
 // Display detail page for a specific book.
